6d41c0a9e873a41d1cf352de069fd22a4e23372b
xuezechao1 <15510726991@163.com>
2022-08-30 17:29:50 +0800

    增加灰度插件dubbo演示应用

---
+++ router-demo/dubbo-router-demo/dubbo-a/src/main/java/com/huawei/dubbotest/service/imp/ATestImp.java
@@ -0,0 +1,47 @@
+package com.huawei.dubbotest.service.imp;
+
+import com.huawei.dubbotest.domain.Test;
+import com.huawei.dubbotest.service.ATest;
+import com.huawei.dubbotest.service.BTest;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ATestImp implements ATest {
+
+    @Resource(name = "bTest")
+    private BTest bTest;
+
+    @Override
+    public Map<String, String> testEmpty(long id, String name) {
+        return bTest.testEmpty(id, name);
+    }
+
+    @Override
+    public Map<String, String> testObject(Test test) {
+        return bTest.testObject(test);
+    }
+
+    @Override
+    public Map<String, String> testEnabled(Test test) {
+        return bTest.testEnabled(test);
+    }
+
+    @Override
+    public Map<String, String> testArray(String[] array) {
+        return bTest.testArray(array);
+    }
+
+    @Override
+    public Map<String, String> testList(List<String> list) {
+        return bTest.testList(list);
+    }
+
+    @Override
+    public Map<String, String> testMap(Map<String, String> map) {
+        return bTest.testMap(map);
+    }
+}
---
+++ router-demo/dubbo-router-demo/spring-cloud-gateway/src/main/java/com/huawei/dubbotest/controller/DubboController.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2021-2021 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huawei.dubbotest.controller;
+
+import com.huawei.dubbotest.domain.Test;
+import com.huawei.dubbotest.service.ATest;
+import org.apache.dubbo.rpc.RpcContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+@RestController
+public class DubboController {
+    @Resource(name = "aTest")
+    private ATest aTest;
+
+    @GetMapping("/empty")
+    public String empty(Long id, String name, @RequestHeader Map<String, String> headers) {
+        RpcContext.getContext().setAttachments(headers);
+        if (id == null) {
+            id = 0L;
+        }
+        if (name == null) {
+            name = "";
+        }
+        Map<String, String> resultMap = new TreeMap<String, String>();
+        resultMap.put("_dubbo.version", "2.7.5");
+        try {
+            resultMap.putAll(aTest.testEmpty(id, name));
+        } catch (Exception e) {
+            resultMap.put("error", e.getMessage());
+        }
+        return resultMap.toString();
+    }
+
+    @GetMapping("/object")
+    public String object(Test test, @RequestHeader Map<String, String> headers) {
+        RpcContext.getContext().setAttachments(headers);
+        Map<String, String> resultMap = new TreeMap<String, String>();
+        resultMap.put("_dubbo.version", "2.7.5");
+        try {
+            resultMap.putAll(aTest.testObject(test));
+        } catch (Exception e) {
+            resultMap.put("error", e.getMessage());
+        }
+        return resultMap.toString();
+    }
+
+    @GetMapping("/enabled")
+    public String enabled(Test test, @RequestHeader Map<String, String> headers) {
+        RpcContext.getContext().setAttachments(headers);
+        Map<String, String> resultMap = new TreeMap<String, String>();
+        resultMap.put("_dubbo.version", "2.7.5");
+        try {
+            resultMap.putAll(aTest.testEnabled(test));
+        } catch (Exception e) {
+            resultMap.put("error", e.getMessage());
+        }
+        return resultMap.toString();
+    }
+
+    @GetMapping("/array")
+    public String array(String name, @RequestHeader Map<String, String> headers) {
+        RpcContext.getContext().setAttachments(headers);
+        String[] arr = null;
+        if (name != null) {
+            arr = name.split(",");
+        }
+        Map<String, String> resultMap = new TreeMap<String, String>();
+        resultMap.put("_dubbo.version", "2.7.5");
+        try {
+            resultMap.putAll(aTest.testArray(arr));
+        } catch (Exception e) {
+            resultMap.put("error", e.getMessage());
+        }
+        return resultMap.toString();
+    }
+
+    @GetMapping("/list")
+    public String list(String name, @RequestHeader Map<String, String> headers) {
+        RpcContext.getContext().setAttachments(headers);
+        List<String> list = null;
+        if (name != null) {
+            list = Arrays.asList(name.split(","));
+        }
+        Map<String, String> resultMap = new TreeMap<String, String>();
+        resultMap.put("_dubbo.version", "2.7.5");
+        try {
+            resultMap.putAll(aTest.testList(list));
+        } catch (Exception e) {
+            resultMap.put("error", e.getMessage());
+        }
+        return resultMap.toString();
+    }
+
+    @GetMapping("/map")
+    public String map(@RequestParam Map<String, String> param, @RequestHeader Map<String, String> headers) {
+        RpcContext.getContext().setAttachments(headers);
+        System.out.println("param:" + param);
+        Map<String, String> resultMap = new TreeMap<String, String>();
+        resultMap.put("_dubbo.version", "2.7.5");
+        try {
+            resultMap.putAll(aTest.testMap(param));
+        } catch (Exception e) {
+            resultMap.put("error", e.getMessage());
+        }
+        return resultMap.toString();
+    }
+}
